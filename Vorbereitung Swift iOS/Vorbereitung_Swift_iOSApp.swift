65fe6141806e3d18b47916f56a863e1569053e5b
Okan Leenen <okan.leenen@syntax-institut.de>
2024-07-26 12:18:09 +0200

    Initial Commit

---
+++ Vorbereitung Swift iOS/Vorbereitung_Swift_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vorbereitung_Swift_iOSApp.swift
+//  Vorbereitung Swift iOS
+//
+//  Created by Okan Leenen on 26.07.24.
+//
+
+import SwiftUI
+
+@main
+struct Vorbereitung_Swift_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
